841ee6524de845615fe203cff941394fd1cc225c
in <in@in.in>
2018-07-09 16:12:34 +0300

    новый файл:    xx.cpp

---
+++ xx.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int sqrtArina(int myNum){
+return myNum*myNum;
+}  
+
+void testsqrtArina()
+{
+cout<<sqrtArina(2);
+}
+
+int main(){
+testsqrtArina();
+
+}
+
+
+
+
+
