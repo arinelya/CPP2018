696f32f12b659dd3c879c8a6148ae5410247c265
in <in@in.in>
2018-07-11 14:40:31 +0300

    Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- power.cpp
+++ power.cpp
@@ -1,14 +1,26 @@
+/*! \file */
 #include <iostream>
+//! \fn Raise int to specific power
+/*!
+ \param iBase Integer to raise in specific power
+ \param power Power
+ \return result of operation
+*/
+int raisePower (int iBase,int power)
+{
+		int iResult=1;
+		for(int i2=1;i2<=power;++i2)
+			iResult*=iBase;
+			return iResult;
+}
 int main()
 {
-	int iBase=2;
-	for(int i1=0;i1<10;++i1)
-	{
-		int iResult;
-		iResult=1;
-		for(int i2=1;i2<=i1;++i2)
-			iResult=iResult*iBase;
-			std::cout<<iResult<<" ";
+        int iBase=2,iResult;
+        for(int i1=0;i1<10;++i1)
+        {
+
+	iResult = raisePower(iBase,i1);
+        std::cout<<iResult<<" ";
 	}
 	std::cout<<std::endl;
 	return 0;
