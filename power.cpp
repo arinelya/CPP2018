6f7048c336a13abf3738a8415411b1332421e025
in <in@in.in>
2018-07-13 01:08:40 +0300

    Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- power.cpp
+++ power.cpp
@@ -6,22 +6,20 @@
  \param power Power
  \return result of operation
 */
-int raisePower (int iBase,int power)
+int reisePower (int iBase,int power)
 {
 		int iResult=1;
 		for(int i2=1;i2<=power;++i2)
 			iResult*=iBase;
 			return iResult;
 }
-int main()
-{
-        int iBase=2,iResult;
-        for(int i1=0;i1<10;++i1)
-        {
-
-	iResult = raisePower(iBase,i1);
-        std::cout<<iResult<<" ";
-	}
-	std::cout<<std::endl;
+void Test(int power, int iBase){
+        for(int i=0;i<power;++i)
+       		std::cout<<reisePower(i,iBase)<<" ";
+        std::cout<<std::endl;
+}
+int main(){
+        Test(10,2);
+	Test(7,3);
 	return 0;
 }
